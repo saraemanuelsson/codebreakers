79257d1f12930d7536ac29d3271d32011b418d5b
Rumen Roger <rumenroger@gmail.com>
2020-07-04 12:00:09 +0100

    Add data in App and create shuffle method

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <grid :cards="cards"></grid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Grid from "./components/Grid.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    "grid": Grid
+  },
+  data() {
+    return {
+      cards: [
+        {colour: 'Red',
+        word: "Mammoth"},
+        {colour: 'Red',
+        word: "Beethoven"},
+        {colour: 'Red',
+        word: "Big"},
+        {colour: 'Red',
+        word: "Bird"},
+        {colour: 'Red',
+        word: "Boat"},
+        {colour: 'Red',
+        word: "Crow"},
+        {colour: 'Red',
+        word: "Dart"},
+        {colour: 'Red',
+        word: "Day"},
+        {colour: 'Red',
+        word: "Mascot"},
+        {colour: 'Blue',
+        word: "Mexico"},
+        {colour: 'Blue',
+        word: "Midsummer"},
+        {colour: 'Blue',
+        word:"Mouse"},
+        {colour: 'Blue',
+        word:"Cat"},
+        {colour: 'Blue',
+        word:"Lake"},
+        {colour: 'Blue',
+        word:"Sky"},
+        {colour: 'Blue',
+        word:"Plane"},
+        {colour: 'Blue',
+        word:"Moon"},
+        {colour: 'Neutral',
+        word:"Mouse"},
+        {colour: 'Neutral',
+        word: 'calendar'},
+        {colour: 'Neutral',
+        word: 'monitor'},
+        {colour: 'Neutral',
+        name: 'guitar'},
+        {colour: 'Neutral',
+        word: 'desk'},
+        {colour: 'Neutral',
+        word: 'alligator'},
+        {colour: 'Neutral',
+        word: 'dog'},
+        {colour: 'Black',
+        word: 'apple'}
+      ]
+    }
+  },
+  mounted() {
+    this.shuffle();
+  },
+  methods: {
+    shuffle() {
+      const shuffled = this.cards.sort(() => 0.5 - Math.random());
+      return shuffled;
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
