c7316fb1c0044711141620b59dbaa0cdfe294993
Rumen Roger <rumenroger@gmail.com>
2020-07-04 15:44:19 +0100

    Words, Pre Play Cards and Remaining Cards Insomnia tests passed

--- server/server.js
+++ server/server.js
@@ -14,12 +14,16 @@ MongoClient.connect('mongodb://localhost:27017')
     const db = player.db('codebreaker');
     const prePlayCardsCollection = db.collection('prePlayCards');
     const wordsCollection = db.collection('words');
+    const remainingCardsCollection = db.collection('remainingCards');
 
     const prePlayCardsRouter = createRouter(prePlayCardsCollection);
     app.use('/api/preplaycards', prePlayCardsRouter);
 
     const wordsRouter = createRouter(wordsCollection);
     app.use('/api/words', wordsRouter);
+
+    const remainingCardsRouter = createRouter(remainingCardsCollection);
+    app.use('/api/remainingcards', remainingCardsRouter);
   })
   .catch(console.err);
 
