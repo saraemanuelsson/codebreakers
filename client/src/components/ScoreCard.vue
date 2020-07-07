9f20e91a6bd6ded609847783f65afe2e9014399c
AdamK <44851323+adamski10@users.noreply.github.com>
2020-07-07 15:42:06 +0100

    cards disappear on click

--- client/src/components/ScoreCard.vue
+++ client/src/components/ScoreCard.vue
@@ -1,10 +1,25 @@
 <template>
 <div id="score-wrapper">
   <div id="redwrapper">
-      <h2 id="redcards">{{redScore}}</h2>
+      <h2 v-if="redScore > 1" class="redcards"></h2>
+      <h2 v-if="redScore > 2" class="redcards"></h2>
+      <h2 v-if="redScore > 3" class="redcards"></h2>
+      <h2 v-if="redScore > 4" class="redcards"></h2>
+      <h2 v-if="redScore > 5" class="redcards"></h2>
+      <h2 v-if="redScore > 6" class="redcards"></h2>
+      <h2 v-if="redScore > 7" class="redcards"></h2>
+      <h2 v-if="redScore > 8" class="redcards"></h2>
+      <h2 id="redScoreCard" class="redcards">{{redScore}}</h2>
   </div>
   <div id="bluewrapper">
-      <h2 id="bluecards">{{blueScore}}</h2>
+      <h2 v-if="blueScore > 1" class="bluecards"></h2>
+      <h2 v-if="blueScore > 2" class="bluecards"></h2>
+      <h2 v-if="blueScore > 3" class="bluecards"></h2>
+      <h2 v-if="blueScore > 4" class="bluecards"></h2>
+      <h2 v-if="blueScore > 5" class="bluecards"></h2>
+      <h2 v-if="blueScore > 6" class="bluecards"></h2>
+      <h2 v-if="blueScore > 7" class="bluecards"></h2>
+      <h2 id="blueScoreCard" class="bluecards">{{blueScore}}</h2>
   </div>
 </div>
 </template>
@@ -44,7 +59,9 @@ export default {
     grid-column: 1/2;
 }
 
-#redcards{
+
+.redcards{
+    display: flex;
     box-shadow: -16px -1px 16px -4px rgba(0,0,0,0.75);
     color:white;
     height: 4vw;
@@ -53,16 +70,23 @@ export default {
     background-color: rgb(139, 0, 0);
     border-radius: 15%;
     text-align: center;
-    margin: 0 !important;
+    margin: -20px !important;
     padding: 0 !important;
 }
+
+h2 > #redScoreCard{
+    color: ;
+    text-align: center;
+    padding: 10px;
+}
+
 #bluewrapper{
     display: flex;
     flex-direction: row-reverse;
     grid-column: 3/4;
 }
 
-#bluecards{
+.bluecards{
     color: rgb(105, 105, 105);
     height: 4vw;
     width: 4vw;
@@ -71,7 +95,7 @@ export default {
     background-color: rgba(10, 136, 177, 0.838);
     text-align: center;
     color: white;
-    margin: 0 !important;
+    margin: -20px !important;
     padding: 0 !important;
 
 }
